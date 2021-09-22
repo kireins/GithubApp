63be318532c3383aaa4c5afd2661a61263dcf60d
kireins <kireishafira@gmail.com>
2021-09-22 11:55:46 +0700

    Initial commit

---
+++ app/src/main/java/com/kirei/githubapp/activity/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.kirei.githubapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kirei.githubapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
---
+++ app/src/main/java/com/kirei/githubapp/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kirei.githubapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kirei.githubapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
